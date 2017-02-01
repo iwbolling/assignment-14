68915038a9d25886b6345dee04b6aa700826ef8d
iwbolling <iwbolling@gmail.com>
2017-01-31 19:36:55 -0500

    completed task 6, unsure of efficiency

--- main.js
+++ main.js
@@ -57,9 +57,27 @@ document.querySelector("#remove button").addEventListener('click',function(){
   })
 })
 
-
 document.querySelector("#reverse-squares button").addEventListener('click',function(){
   // TASK #6
+  var colorSquares = document.querySelectorAll(".answer-box span");
+  var colorsBox = document.querySelector(".answer-box")
+  if (colorSquares[0].className === "square s3") {
+    colorSquares[0].className = "square s2";
+    colorSquares[1].className = "square s1";
+    colorSquares[2].className = "square s2";
+    colorSquares[3].className = "square s1";
+    colorSquares[4].className = "square s1";
+    colorSquares[5].className = "square s1";
+    colorSquares[6].className = "square s3";
+  } else {
+    colorSquares[0].className = "square s3";
+    colorSquares[1].className = "square s1";
+    colorSquares[2].className = "square s1";
+    colorSquares[3].className = "square s1";
+    colorSquares[4].className = "square s2";
+    colorSquares[5].className = "square s1";
+    colorSquares[6].className = "square s2";
+  }
 })
 
 document.querySelector("#pig-latin button").addEventListener('click',function(){
